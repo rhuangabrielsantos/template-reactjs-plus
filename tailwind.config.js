fa01c84928198f860eb2facb3ae407a63a08523c
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2021-08-08 17:03:25 -0300

    :memo: Create README

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false,
   theme: {
     extend: {},
     colors: {
-      transparent: "transparent",
-      current: "currentColor",
+      transparent: 'transparent',
+      current: 'currentColor',
       ...colors,
     },
     fontFamily: {
-      roboto: ["Roboto", "ui-sans-serif", "system-ui"],
+      roboto: ['Roboto', 'ui-sans-serif', 'system-ui'],
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms"), require("tailwind-dracula")()],
+  plugins: [require('@tailwindcss/forms'), require('tailwind-dracula')()],
 };
