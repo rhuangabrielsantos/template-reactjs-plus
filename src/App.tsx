e141503e8106d8be35b705c9c831e5d1d4d7b256
Rhuan Gabriel Santos <rhuan.santos@db1.com.br>
2021-08-08 15:17:20 -0300

    :lipstick: Added tailwind lib with dracula theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
+    <div className="flex items-center justify-center bg-nosferatu w-screen h-screen">
+      <h1 className="text-cullen text-3xl font-roboto font-light">
+        Boilerplate tailwind with react.js
+      </h1>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      ...colors,
+    },
+    fontFamily: {
+      roboto: ["Roboto", "ui-sans-serif", "system-ui"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms"), require("tailwind-dracula")()],
+};
